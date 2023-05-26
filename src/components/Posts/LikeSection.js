cbee42550497929f32b172b7f5d902a9cb235930
Joseph Noblett <noblettjoe@gmail.com>
2023-05-26 11:10:26 -0500

    fixed the hard coded likes, then added onclick so the button works

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -2,14 +2,15 @@
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
-  const { likePost, numberOfLikes } = props;
-
+  const { likePost, post } = props;
+  const [isLiked, setIsLiked] = useState(false);
+  console.log(post);
   return (
     <div>
       <div
@@ -17,13 +18,18 @@ const LikeSection = props => {
         key='likes-icons-container'
       >
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} />
+          <button onClick={() => {
+              if (!isLiked) {
+                likePost(post.id);
+                setIsLiked(true);
+              }
+            }}><FontAwesomeIcon icon={faHeart} /></button>
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{props.post.likes}</p>
     </div>
   );
 };
